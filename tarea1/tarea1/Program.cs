fa6379e38e0f5199186f80ff9d2050afddf83dcb
sandra ventura <sandraventura633@gmail.com>
2024-07-24 09:22:29 -0600

    ejercicio numero 3

--- tarea1/tarea1/Program.cs
+++ tarea1/tarea1/Program.cs
@@ -10,6 +10,31 @@ namespace tarea1
     {
         static void Main(string[] args)
         {
+            Console.Write(" num 1:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("num 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            int res = num1 + num2;
+            Console.WriteLine(" la suma {0}+{1}={2}", num1, num2, res);
+
+            int num = -1;
+            string msg = "";
+            if (num <= 0)
+            {
+                msg = "no Puede colocar ese valor en el numero 1";
+
+            }
+
+            Console.WriteLine(msg);
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
