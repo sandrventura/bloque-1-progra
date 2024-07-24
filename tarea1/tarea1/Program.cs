b2d3b680600936d5543c2a92c1637bb5cbe3d5f3
sandra ventura <sandraventura633@gmail.com>
2024-07-24 11:39:35 -0600

    ejercicio numero 1

--- tarea1/tarea1/Program.cs
+++ tarea1/tarea1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,31 +12,31 @@ namespace tarea1
     {
         static void Main(string[] args)
         {
-            Console.Write(" num 1:");
-            int num1 = int.Parse(Console.ReadLine());
-
-            Console.Write("num 2:");
-            int num2 = int.Parse(Console.ReadLine());
-
-
-            int res = num1 + num2;
-            Console.WriteLine(" la suma {0}+{1}={2}", num1, num2, res);
-
-            int num = -1;
-            string msg = "";
-            if (num <= 0)
+            while (true)
             {
-                msg = "no Puede colocar ese valor en el numero 1";
+                Console.WriteLine(" ingrese un numero");
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero == 0)
+                {
+                    Console.WriteLine("");
+                }
+
+                if (numero > 0)
+
+                {
+                    Console.WriteLine("es numero positivo");
+                }
+
+                else
+                {
+                    Console.WriteLine(" el numero es negativo");
+                    Console.WriteLine("finalizado");
+                }
 
             }
-
-            Console.WriteLine(msg);
-
-
-            Console.ReadLine();
-
-
 
         }
     }
 }
+        
