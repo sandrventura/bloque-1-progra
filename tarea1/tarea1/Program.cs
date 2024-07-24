538f9eb23fa23bbc598f4cf79e8d57b9655c150e
sandra ventura <sandraventura633@gmail.com>
2024-07-24 16:39:28 -0600

    creacion de Rama

--- tarea1/tarea1/Program.cs
+++ tarea1/tarea1/Program.cs
@@ -4,43 +4,13 @@ namespace tarea1
 {
     internal class Program
     {
-        
-            static void Main(string[] args)
-            {
-                double suma = 0;
-                int contador = 0;
 
-                double numero;
-
-                do
-                {
-                    Console.WriteLine("Ingrese un número (0 para finalizar):");
-                    numero = double.Parse(Console.ReadLine());
-
-                    if (numero != 0)
-                    {
-                        suma += numero;
-                        contador++;
-                    }
-                } while (numero != 0);
-
-                if (contador > 0)
-                {
-                    double promedio = suma / contador;
-                    Console.WriteLine($"El promedio de los números ingresados es: {promedio}");
-                }
-                else
-                {
-                    Console.WriteLine("No se ingresaron números.");
-                Console.ReadLine();
-                }
-            }
+        static void Main(string[] args)
+        {
         }
+    }
+}
 
-
-
-            }
-  
 
 
 
