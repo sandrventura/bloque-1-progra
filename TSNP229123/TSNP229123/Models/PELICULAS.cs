273648041f23e641c4b1fe89f035cfa5993cd6e0
sandra ventura <sandraventura633@gmail.com>
2024-07-31 15:18:34 -0600

    creacion de aplicacion

--- TSNP229123/TSNP229123/Models/PELICULAS.cs
+++ TSNP229123/TSNP229123/Models/PELICULAS.cs
@@ -6,11 +6,11 @@ namespace TSNP229123.Models
         [Key]
         public int IdPeliculas{ get; set; }
         public int CodigodePelicula { get; set; }
-        public int Titulo { get; set; }
-        public int Autor { get; set; }
-        public int Sinopsis { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public string Sinopsis { get; set; }
         public int Duracion { get; set; }
-        public int Clasificacion { get; set; }
+        public string Clasificacion { get; set; }
 
     }
 }
