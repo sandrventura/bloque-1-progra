9838d7920e75086c55f17b9c7dc4cc946c4d7c2c
sandra ventura <sandraventura633@gmail.com>
2024-07-31 11:50:21 -0600

    CREACION RAMA 3 APLICACION

---
+++ TSNP229123/TSNP229123/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSNP229123.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
