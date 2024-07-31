390a0fbeeea3cbf0a63185cc9aa10b34aa3348d9
sandra ventura <sandraventura633@gmail.com>
2024-07-31 13:05:59 -0600

    creaccion de aplicacion con Entity

---
+++ TSNP229123/TSNP229123/Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TSNP229123.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<PELICULAS> PELICULAS { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PELICULAS>().HasKey(c => c.IdPeliculas);
+        }
+    }
+}
---
+++ TSNP229123/TSNP229123/Models/PELICULAS.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace TSNP229123.Models
+{
+    public class PELICULAS
+    {
+        [Key]
+        public int IdPeliculas { get; set; }
+        public int CodigodePelicula { get; set; }
+        public int Titulo { get; set; }
+        public int Autor { get; set; }
+        public int Sinopsis { get; set; }
+        public int Duracion { get; set; }
+        public int Clasificacion { get; set; }
+
+    }
+}
