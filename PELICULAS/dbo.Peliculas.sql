a2b276e1256eb2a3df7521e862276d02c4947d47
sandra ventura <sandraventura633@gmail.com>
2024-07-31 19:38:03 -0600

    creacion aplicacion Entity

---
+++ PELICULAS/dbo.Peliculas.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Peliculas] (
+    [IdPeliculas]        INT        IDENTITY (1, 1) NOT NULL,
+    [Codigo de Pelicula] CHAR (10)  NULL,
+    [Titulo]             CHAR (20)  NOT NULL,
+    [Autor]              CHAR (20)  NULL,
+    [Sinopsis]           CHAR (150) NOT NULL,
+    [Duracion]           INT   NULL,
+    [Clasificacion]      CHAR (5)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdPeliculas] ASC)
+);
+
--- TSNP229123/TSNP229123/Models/MyDbContext.cs
+++ TSNP229123/TSNP229123/Models/MyDbContext.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+
 namespace TSNP229123.Models
 {
     public class MyDbContext : DbContext
     {
-        public  MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
-        public DbSet<PELICULAS> PELICULAS { get; set; }
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<PELICULAS> Peliculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<PELICULAS>().HasKey(c => c.IdPeliculas);
         }
     }
-}
+    }
+
