ef675cb1cf7ce2b0d9ffc8d63dd7bb00a8647a39
sandra ventura <sandraventura633@gmail.com>
2024-08-09 12:00:20 -0600

    aplicacion framework

---
+++ TSNP204023_TSNP210223_TSNP229123unidad3/TSNP204023_TSNP210223_TSNP229123unidad3/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TSNP204023_TSNP210223_TSNP229123unidad3.Models;
+namespace TSNP204023_TSNP210223_TSNP229123UNIDAD3.Models
+{
+    public class MyDbContext : DbContext{
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Peliculas> peliculas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Peliculas>().HasKey(c => c.IdPelicula);
+        }
+
+
+
+
+    }
+}
