252af2978da7fa1873d0ab1b9b576f7ca548babc
sandra ventura <sandraventura633@gmail.com>
2024-08-09 11:16:34 -0600

    CREACION DE APLICACION ENTITY FRAMEWORK

---
+++ TSNP204023_TSNP210223_TSNP229123unidad3/TSNP204023_TSNP210223_TSNP229123unidad3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSNP204023_TSNP210223_TSNP229123UNIDAD3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
