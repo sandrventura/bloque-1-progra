06eff27270877c0904a159cc2062b06f5e6e1e05
SANDRA VENTURA <159978861+sandrventura@users.noreply.github.com>
2024-08-09 19:53:31 -0600

    Delete TSNP204023_TSNP210223_TSNP229123unidad3/TSNP204023_TSNP210223_TSNP229123unidad3 directory
    NO SIRVE

--- TSNP204023_TSNP210223_TSNP229123unidad3/TSNP204023_TSNP210223_TSNP229123unidad3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TSNP204023_TSNP210223_TSNP229123UNIDAD3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
